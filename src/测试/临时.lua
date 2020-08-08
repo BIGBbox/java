2cbe48ad3011d45d6a3d1fd668db8cb00aa90a1a
BigBbox <a1156514343@gmail.com>
2020-08-08 10:48:45 +0800

    新加一个文件

---
+++ src/测试/临时.lua
@@ -0,0 +1,23 @@
+--[[将多位数字的每一位存入到一个数组内
+	数组中索引1的数字是最高位数字
+
+]]
+function getWeightNum(num)
+	local t = {}
+	if tonumber(num) == nil then
+		return t
+	end
+
+	local length = #tostring(num)
+	for i = length, 1, -1 do
+		local s = math.floor(num / (10 ^ (i - 1)) % 10)
+		table.insert(t, s)
+	end
+
+	return t
+end
+
+--local num = 98764
+--local t = getWeightNum(num)
+--t = {9,8,7,6,4}
+
